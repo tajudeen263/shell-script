7f2f9bcb0ac95c12dbda13053640a5aa538819bf
tajudeen263 <oluwaseun263@yahoo.com>
2020-02-05 09:29:18 -0800

    pushhh

--- variable.sh
+++ variable.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-Date=$(date +%F)
-echo Hello, good morning. Today date is $Date
+read -p 'enter the username to add:'user_name
+read -p 'enter the groupname to add'group_name
+echo "the above statement to be executed as useradd -g $user_name $group_name"
