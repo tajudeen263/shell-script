df8c7a367928c819020e7bf884edb51b970e9fb3
tajudeen263 <oluwaseun263@yahoo.com>
2020-02-07 10:07:01 -0800

    new topic

--- variable.sh
+++ variable.sh
@@ -1,4 +1,2 @@
 #! /bin/bash
-read -p 'enter the username to add:'user_name
-read -p 'enter the groupname to add'group_name
-echo "the above statement to be executed as useradd -g $user_name $group_name"
+echo $0
